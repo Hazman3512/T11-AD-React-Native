cf14006bfb0ab9b56b0a5de2aa15430bdfd4b60a
Hazman3512 <hazman3512@gmail.com>
2021-08-06 17:17:11 +0800

    Update historyDetails.js

--- screens/historyDetails.js
+++ screens/historyDetails.js
@@ -7,6 +7,7 @@ export default function About( { navigation } ){
     return(
         <View style={globalStyles.container}>
             <Text h1>Alert History for all</Text>
+            <Text h1>Testing 1 2 3</Text>
             
             
         </View>
