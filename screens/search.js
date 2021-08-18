ff7e0faaefd71dd0253cb8e8554a3177588f62e1
wanlingt <wanling210@gmail.com>
2021-08-18 21:47:52 +0800

    updated with search

--- screens/search.js
+++ screens/search.js
@@ -1,35 +1,88 @@
 import React from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import { useState, useEffect } from 'react'
+import { FlatList } from 'react-native';
+import { ScrollView } from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { Button,Searchbar } from 'react-native-paper';
+import { Button, Searchbar } from 'react-native-paper';
+import ChartService from '../services/ChartService';
 
 
 export default function Search({navigation, route}) {
 
 
-  const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchTimer, setSearchTimer] = useState(null);
 
   const onChangeSearch = query => setSearchQuery(query);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [stockInfo, setStockInfo] = useState({
+    stockTicker: null,
+    companyName: null,
+    initialPrice: null,
+    sentiment: null
+  });
+
+  async function getInitialPrice(ticker) {
+
+    const req = await ChartService.getLatestClosingStockPrice(ticker);
+    const sentimentReq = await ChartService.getLatestStockSentiment(ticker);
+    const latestPrice = parseFloat(req.data.close);
+    const company = req.data.description;
+    const sentimentData = sentimentReq.data;
+
+    setIsLoading(() => {
+      setStockInfo({
+      stockTicker: ticker,
+      companyName: company,
+      initialPrice: latestPrice,
+      sentiment: sentimentData
+  });
+  return false;});
+
+  }
 
 
     return (
-      
-        <View style={{flex:1,backgroundColor:'white'}} >
+      <ScrollView>
+        <View style={styles.container} >
         <Searchbar
         style={{borderRadius:5}}
         placeholder="Search Stocks"
-        onChangeText={onChangeSearch}
+        onChangeText={(text) => {
+          if (searchTimer) {
+            clearTimeout(searchTimer);
+          }
+          setSearchQuery(text);
+          setSearchTimer(
+            setTimeout(() => {
+              getInitialPrice(text);
+            }, 2000),
+          );
+        }}
         value={searchQuery}
         onIconPress={()=> console.log({searchQuery})}
         />
-
-          <Text> {searchQuery} </Text>
-
         </View>
-
-        
-
-     
-    )
+        <View style = {styles.container}>
+          <Text>{stockInfo.stockTicker}</Text>
+          <Text>{stockInfo.companyName}</Text>
+          <Text>{stockInfo.initialPrice}</Text>
+          <Text>Sentiment: {stockInfo.sentiment}</Text> 
+          </View>
+      </ScrollView>
+    );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        paddingHorizontal: 6,
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "flex-start",
+    },
+});
--- services/ChartService.js
+++ services/ChartService.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
-const STOCK_CANDLE_API_URL = 'http://localhost:8082/getCandleData/';
-const STOCK_PRICE_API_URL = 'http://localhost:8082/getLatestPrice/';
+// const STOCK_CANDLE_API_URL = 'http://localhost:8082/getCandleData/';
+// const STOCK_PRICE_API_URL = 'http://localhost:8082/getLatestPrice/';
+const STOCK_CANDLE_API_URL = "http://192.168.1.137:5000/getCandleData/";
+const STOCK_PRICE_API_URL = "http://192.168.1.137:5000/getLatestPrice/";
+const STOCK_SENTIMENT_API_URL = "http://192.168.1.137:5000/getSentiment/";
+const STOCK_TOP5_API_URL = "http://192.168.1.137:5000/getStockSymbol/";
+
 // https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000
 class ChartService {
 
@@ -14,6 +19,16 @@ class ChartService {
         const url = STOCK_PRICE_API_URL + ticker; 
         return axios.get(url);
     }
+
+    getLatestStockSentiment(ticker) {
+        const url = STOCK_SENTIMENT_API_URL + ticker;
+        return axios.get(url);
+      }
+    
+      getTop5Stock(searchTerm) {
+        const url = STOCK_TOP5_API_URL + searchTerm;
+        return axios.get(url);
+      }
 }
 
 export default new ChartService();
