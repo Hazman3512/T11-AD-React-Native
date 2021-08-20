45d6ec9a8f30df23dcfead04042c864e3b21e416
Hazman3512 <hazman3512@gmail.com>
2021-08-20 12:36:31 +0800

    Update Alert History page
    Added Scan button and dropdown to pick stock

--- screens/history.js
+++ screens/history.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import {TouchableOpacity, StyleSheet, View } from "react-native";
 import {Button,Subheading,Text, Card, Title, Paragraph } from "react-native-paper";
-
+import {Picker} from '@react-native-picker/picker';
 
 
 
 export default function History({ navigation }){
+
+  const [selectedStock, setSelectedStock] = useState("Choose Stock");
 
 
     return (
@@ -13,16 +15,28 @@ export default function History({ navigation }){
 
         <View style={styles.container}>
           <Title style={{alignSelf:'center',marginTop:20}}>Alert History</Title>
-          <TouchableOpacity>
+          <Picker
+            style={{marginHorizontal:70,marginTop:20}}
+            mode='dropdown'
+            selectedValue={selectedStock}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedStock(itemValue)
+            }>
+            <Picker.Item label="AAPL" value="apple" />
+            <Picker.Item label="GOOG" value="bynd" />
+          </Picker>
+
+
+          
           <Button
-            style={{marginTop:40,marginHorizontal:70}} 
+            style={{marginTop:440,marginHorizontal:70}} 
             color="#1e3a8a" 
             icon="chart-bar" 
             mode="contained" 
             onPress={() => console.log('Pressed')}>
             SCAN
           </Button>
-          </TouchableOpacity>
+          
           
         </View>
     )
