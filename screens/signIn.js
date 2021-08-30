b7dc134263a5b73867a155c65ded36204e980e39
ZavierLim <75073384+ZavierLim@users.noreply.github.com>
2021-08-30 19:57:01 +0800

    removed right button from register and login

--- screens/signIn.js
+++ screens/signIn.js
@@ -1,162 +1,147 @@
-import React, {useState} from 'react';
-import {   Alert,StyleSheet, TouchableOpacity, View } from 'react-native';
-import { Title,Text, Button, TextInput } from 'react-native-paper';
+import React, { useState } from "react";
+import { Alert, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Title, Text, Button, TextInput } from "react-native-paper";
 import UserService from "../services/UserService";
 import { Avatar } from "react-native-elements";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import WatchlistService from '../services/WatchlistService';
+import WatchlistService from "../services/WatchlistService";
 
+export default function SignIn({ navigation }) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isSuccess, setIsSuccess] = React.useState(true);
 
-export default function SignIn({ navigation }){
-    
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [isSuccess, setIsSuccess] = React.useState(true);
+  //Authentication code
+  const loginSuccessOrFail = async (response) => {
+    if (response.status == 200) {
+      AsyncStorage.setItem("username", username);
+      //store user pre-saved watchlist
+      try {
+        const watchlist = await WatchlistService.getStockWatchlist(username);
+        AsyncStorage.setItem("watchlist", JSON.stringify(watchlist.data));
+        console.log(await AsyncStorage.getItem("watchlist"));
 
-
-    //Authentication code
-    const loginSuccessOrFail= async (response)=>{
-        if(response.status == 200){
-        AsyncStorage.setItem('username', username);
-        //store user pre-saved watchlist
-        try{
-            const watchlist = await WatchlistService.getStockWatchlist(username);
-            AsyncStorage.setItem('watchlist', JSON.stringify(watchlist.data));
-            console.log(await AsyncStorage.getItem('watchlist'));
-            
-       
-        navigation.navigate('MainTabScreen', {screen: 'Search'});
-        }catch(error){
-            console.log(error);
-            
-        }
-
-        }
-        else{
-            isSuccess(false);
-          setIsSuccess(false);
-        
-        }
-        return;
-      };
-
-     function validateForm() {
-         if (username == "" || password == ""){
-             Alert.alert("Wrong Input","Username of password field cannot be empty!", [{text: 'OK'}]);
-             return false
-         }if (isSuccess == false)
-         {
-             Alert.alert("Incorrect Input", "Username or password is incorrect!", [{text: 'OK'}]);
-             return false
-         }
-         return true
-     }
-
-     function handleError(){
-        if (isSuccess == false)
-        {
-            Alert.alert("Incorrect Input", "Username or password is incorrect!", [{text: 'OK'}]);
-            return false;
-        }
-        return true;
-
-     }
-
-    const handleSubmit =  async()  => {
-        try{
-        validateForm();
-        const loginuser={username:username,password:password}
-        const req =  await UserService.authenticateUser(loginuser);
-        loginSuccessOrFail(req);
-        
-        }
-        catch(e)
-        {
-            console.log(e);
-
-        }
-        
+        navigation.navigate("MainTabScreen", { screen: "Search" });
+      } catch (error) {
+        console.log(error);
       }
-    
-      return(
-        <View style={styles.container}>
+    } else {
+      isSuccess(false);
+      setIsSuccess(false);
+    }
+    return;
+  };
 
-                <Avatar
-                size="medium"
-                rounded
-                icon={{name: 'lock', type: 'material-icons-outlined'}}
-                activeOpacity={0.7}
-                overlayContainerStyle={{backgroundColor: "#1bbd7e"}}
-                containerStyle={{alignSelf:'center', marginTop: 50,textAlign: 'center'}}
-                />
+  function validateForm() {
+    if (username == "" || password == "") {
+      Alert.alert(
+        "Wrong Input",
+        "Username of password field cannot be empty!",
+        [{ text: "OK" }]
+      );
+      return false;
+    }
+    if (isSuccess == false) {
+      Alert.alert("Incorrect Input", "Username or password is incorrect!", [
+        { text: "OK" },
+      ]);
+      return false;
+    }
+    return true;
+  }
 
-                <Title style={{alignSelf:'center',marginTop:20}}>Sign In</Title>
-                
-                <TextInput style={{marginTop:50,paddingHorizontal:10}}
-                    label="Username"
-                    value={username}
-                    mode='outlined'
-                    onChangeText={username => setUsername(username)}
-                    
-                />
+  function handleError() {
+    if (isSuccess == false) {
+      Alert.alert("Incorrect Input", "Username or password is incorrect!", [
+        { text: "OK" },
+      ]);
+      return false;
+    }
+    return true;
+  }
 
-                <TextInput style={{marginTop:20, paddingHorizontal:10}}
-                    label="Password"
-                    value={password}
-                    mode='outlined'
-                    onChangeText={password => setPassword(password)}
-                    secureTextEntry={true}
-                    right={<TextInput.Icon name="eye" />}
-                />
+  const handleSubmit = async () => {
+    try {
+      validateForm();
+      const loginuser = { username: username, password: password };
+      const req = await UserService.authenticateUser(loginuser);
+      loginSuccessOrFail(req);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-                <TouchableOpacity>
-                <Button style={{marginTop:40,marginHorizontal:10}}
-                    
-                    mode="contained"
-                    color="#1e3a8a"
-                    onPress={handleSubmit} 
+  return (
+    <View style={styles.container}>
+      <Avatar
+        size="medium"
+        rounded
+        icon={{ name: "lock", type: "material-icons-outlined" }}
+        activeOpacity={0.7}
+        overlayContainerStyle={{ backgroundColor: "#1bbd7e" }}
+        containerStyle={{
+          alignSelf: "center",
+          marginTop: 50,
+          textAlign: "center",
+        }}
+      />
 
-                >Login
-                </Button>  
+      <Title style={{ alignSelf: "center", marginTop: 20 }}>Sign In</Title>
 
-                </TouchableOpacity>
+      <TextInput
+        style={{ marginTop: 50, paddingHorizontal: 10 }}
+        label="Username"
+        value={username}
+        mode="outlined"
+        onChangeText={(username) => setUsername(username)}
+      />
 
-                 <Text style={styles.register}>Don't have an account yet? </Text>
-                 <TouchableOpacity>
-                 <Button
-                    color="#1e3a8a" 
-                    mode="text" 
-                    style={{marginTop:20}}
-                    onPress={() => navigation.navigate('Register')}
-                    >Register here</Button>
-                 </TouchableOpacity>
+      <TextInput
+        style={{ marginTop: 20, paddingHorizontal: 10 }}
+        label="Password"
+        value={password}
+        mode="outlined"
+        onChangeText={(password) => setPassword(password)}
+        secureTextEntry={true}
+      />
 
-                 {/* {isSuccess ?  <View />: <View>
+      <TouchableOpacity>
+        <Button
+          style={{ marginTop: 40, marginHorizontal: 10 }}
+          mode="contained"
+          color="#1e3a8a"
+          onPress={handleSubmit}
+        >
+          Login
+        </Button>
+      </TouchableOpacity>
+
+      <Text style={styles.register}>Don't have an account yet? </Text>
+      <TouchableOpacity>
+        <Button
+          color="#1e3a8a"
+          mode="text"
+          style={{ marginTop: 20 }}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Register here
+        </Button>
+      </TouchableOpacity>
+
+      {/* {isSuccess ?  <View />: <View>
                 <Text style={{color:'red', fontSize:20}} >Information above is incorrect. Please try again!
                  </Text></View>} */}
-                    
-                
-            </View>
-        
-
-
-
-    );
-
-
-
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'white'
-    },
-    register:{
-        textAlign: 'center',
-        marginTop:20,   
-        
-    },
-    
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  register: {
+    textAlign: "center",
+    marginTop: 20,
+  },
+});
